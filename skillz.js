d4197734751f6d85356dbae70411478f33e6c912
Jon Low <lowjonc@gmail.com>
2017-01-04 20:59:26 -0700

    added skillz.js

---
+++ skillz.js
@@ -0,0 +1,30 @@
+let skillz = [
+  {
+    "id": 1,
+  "name": "Javascript",
+  "experience": "Intermediate"
+},
+  {
+    "id": 2,
+  "name": "jQuery",
+  "experience": "noob"
+},
+  {
+    "id": 3,
+  "name": "distributing literature",
+  "experience": "expert"
+},
+  {
+    "id": 4,
+  "name": "making fire",
+  "experience": "expert"
+},
+  {
+    "id": 5,
+  "name": "KSP rocketry",
+  "experience": "noob"
+}
+
+]
+
+module.exports = skillz
